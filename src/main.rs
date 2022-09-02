60e4f508bb8255b19dbcaf35fade6eb84cc48a3e
李瑞丰 <liruifeng1024@gmail.com>
2022-09-02 11:17:55 +0800

    feat: 模块化 (#1)
    * feat: fix
    
    * feat: 模块化写法
    
    Co-authored-by: 李瑞丰🤡 <lirf@lenovocloud.com>

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,8 @@
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+mod lib;
+
+use crate::lib::front_of_house::hosting;
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    // println!("The secret number is: {}", secret_number);
-    
-    loop {
-        println!("Please input your guess.");
-        // mut 变量可变 
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess) // & 表示引用
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse(){
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
-
+    hosting::add_to_waitlist();
+    println!("Hello, world!");
 }
