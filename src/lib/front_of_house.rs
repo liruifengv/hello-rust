1c349d5b01936061d66bbdaf96ac72ba25f90382
liruifengv <liruifeng1024@gmail.com>
2022-09-07 10:21:42 +0800

    feat: 调整模块化

--- src/lib/front_of_house.rs
+++ src/lib/front_of_house.rs
@@ -1,8 +1,12 @@
-
 use std::fmt;
 use std::mem;
 #[derive(Debug)]
 struct Matrix(f32, f32, f32, f32);
+
+#[derive(Debug)]
+struct Structure(i32);
+#[derive(Debug)]
+struct Deep(Structure);
 
 impl fmt::Display for Matrix {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -20,8 +24,49 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
 
     (boolean, integer)
 }
-pub fn add_to_waitlist() {
-    println!("add_to_waitlist!");
+pub fn hello_world() {
+    println!("Hello, world!");
+    println!("{} days", 31);
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+    println!("Base 10 repr:               {}", 69420);
+    println!("Base 2 (binary) repr:       {:b}", 69420);
+    println!("Base 8 (octal) repr:        {:o}", 69420);
+    println!("Base 16 (hexadecimal) repr: {:x}", 69420);
+    println!("Base 16 (hexadecimal) repr: {:X}", 69420);
+    println!("{number:>5}", number = 1);
+    println!("{number:0>5}", number = 1);
+    println!("{number:0>width$}", number = 1, width = 5);
+    let number: f64 = 1.0;
+    let width: usize = 6;
+    println!("{number:>width$}");
+    println!("{:?} months in a year.", 12);
+    println!(
+        "{1:?} {0:?} is the {actor:?} name.",
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    // Variables can be type annotated.
+    // 类型推断
+    let logical: bool = true;
+    let mut inferred_type = 12; // Type i64 is inferred from another line
+    inferred_type = 4294967296i64;
+    let mut mutable = 12; // Mutable `i32`
+    mutable = 21;
+    // 变量可以覆盖
+    let mutable = true;
 }
 
 pub fn test_tuple() {
--- src/main.rs
+++ src/main.rs
@@ -5,55 +5,8 @@ use crate::lib::enums;
 use crate::lib::front_of_house;
 use crate::lib::structures;
 
-#[derive(Debug)]
-struct Structure(i32);
-#[derive(Debug)]
-struct Deep(Structure);
-
 fn main() {
-    front_of_house::add_to_waitlist();
-    println!("Hello, world!");
-    println!("{} days", 31);
-    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
-    println!(
-        "{subject} {verb} {object}",
-        object = "the lazy dog",
-        subject = "the quick brown fox",
-        verb = "jumps over"
-    );
-    println!("Base 10 repr:               {}", 69420);
-    println!("Base 2 (binary) repr:       {:b}", 69420);
-    println!("Base 8 (octal) repr:        {:o}", 69420);
-    println!("Base 16 (hexadecimal) repr: {:x}", 69420);
-    println!("Base 16 (hexadecimal) repr: {:X}", 69420);
-    println!("{number:>5}", number = 1);
-    println!("{number:0>5}", number = 1);
-    println!("{number:0>width$}", number = 1, width = 5);
-    let number: f64 = 1.0;
-    let width: usize = 6;
-    println!("{number:>width$}");
-    println!("{:?} months in a year.", 12);
-    println!(
-        "{1:?} {0:?} is the {actor:?} name.",
-        "Slater",
-        "Christian",
-        actor = "actor's"
-    );
-    // `Structure` is printable!
-    println!("Now {:?} will print!", Structure(3));
-    println!("Now {:?} will print!", Deep(Structure(7)));
-
-    println!("My name is {0}, {1} {0}", "Bond", "James");
-
-    // Variables can be type annotated.
-    // 类型推断
-    let logical: bool = true;
-    let mut inferred_type = 12; // Type i64 is inferred from another line
-    inferred_type = 4294967296i64;
-    let mut mutable = 12; // Mutable `i32`
-    mutable = 21;
-    // 变量可以覆盖
-    let mutable = true;
+    front_of_house::hello_world();
 
     front_of_house::test_tuple();
     front_of_house::test_arr();
