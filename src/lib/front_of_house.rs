880be6872229048c51066575e8083e9120769a41
李瑞丰 <liruifeng1024@gmail.com>
2022-09-07 10:23:30 +0800

    feat: 调整模块化 (#6)
    * feat: 调整模块化
    
    * feat: 调整模块化

--- src/lib/front_of_house.rs
+++ src/lib/front_of_house.rs
@@ -1,77 +1,121 @@
-pub mod hosting {
-    use std::fmt;
-    use std::mem;
-    #[derive(Debug)]
-    struct Matrix(f32, f32, f32, f32);
+use std::fmt;
+use std::mem;
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
 
-    impl fmt::Display for Matrix {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            // Write strictly the first element into the supplied output
-            // stream: `f`. Returns `fmt::Result` which indicates whether the
-            // operation succeeded or failed. Note that `write!` uses syntax which
-            // is very similar to `println!`.
-            write!(f, "{},{},{},{}", self.0, self.1, self.2, self.3)
-        }
+#[derive(Debug)]
+struct Structure(i32);
+#[derive(Debug)]
+struct Deep(Structure);
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(f, "{},{},{},{}", self.0, self.1, self.2, self.3)
     }
+}
 
-    fn reverse(pair: (i32, bool)) -> (bool, i32) {
-        // `let` can be used to bind the members of a tuple to variables
-        let (integer, boolean) = pair;
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    // `let` can be used to bind the members of a tuple to variables
+    let (integer, boolean) = pair;
 
-        (boolean, integer)
-    }
-    pub fn add_to_waitlist() {
-        println!("add_to_waitlist!");
-    }
+    (boolean, integer)
+}
+pub fn hello_world() {
+    println!("Hello, world!");
+    println!("{} days", 31);
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+    println!("Base 10 repr:               {}", 69420);
+    println!("Base 2 (binary) repr:       {:b}", 69420);
+    println!("Base 8 (octal) repr:        {:o}", 69420);
+    println!("Base 16 (hexadecimal) repr: {:x}", 69420);
+    println!("Base 16 (hexadecimal) repr: {:X}", 69420);
+    println!("{number:>5}", number = 1);
+    println!("{number:0>5}", number = 1);
+    println!("{number:0>width$}", number = 1, width = 5);
+    let number: f64 = 1.0;
+    let width: usize = 6;
+    println!("{number:>width$}");
+    println!("{:?} months in a year.", 12);
+    println!(
+        "{1:?} {0:?} is the {actor:?} name.",
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
+    println!("Now {:?} will print!", Deep(Structure(7)));
 
-    pub fn test_tuple() {
-        let long_tuple = (
-            1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
-        );
-        // 使用 .index 获取元组里的值
-        println!("long tuple first value: {}", long_tuple.0);
-        println!("long tuple second value: {}", long_tuple.1);
+    println!("My name is {0}, {1} {0}", "Bond", "James");
 
-        // Tuples can be tuple members 多维元组
-        let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+    // Variables can be type annotated.
+    // 类型推断
+    let logical: bool = true;
+    let mut inferred_type = 12; // Type i64 is inferred from another line
+    inferred_type = 4294967296i64;
+    let mut mutable = 12; // Mutable `i32`
+    mutable = 21;
+    // 变量可以覆盖
+    let mutable = true;
+}
 
-        // Tuples are printable ，超过 12 个元素的元组不能被打印
-        println!("tuple of tuples: {:?}", tuple_of_tuples);
+pub fn test_tuple() {
+    let long_tuple = (
+        1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    );
+    // 使用 .index 获取元组里的值
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
 
-        let pair = (1, true);
-        println!("pair is {:?}", pair);
-        println!("the reversed pair is {:?}", reverse(pair));
-        // 创建只有一个元素的元组，需要加逗号
-        println!("one element tuple: {:?}", (5u32,));
-        println!("just an integer: {:?}", (5u32));
-        // 解构元组
-        let tuple = (1, "hello", 4.5, true);
+    // Tuples can be tuple members 多维元组
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
 
-        let (a, b, c, d) = tuple;
-        println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+    // Tuples are printable ，超过 12 个元素的元组不能被打印
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
 
-        let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-        println!("{}", matrix);
-    }
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+    println!("the reversed pair is {:?}", reverse(pair));
+    // 创建只有一个元素的元组，需要加逗号
+    println!("one element tuple: {:?}", (5u32,));
+    println!("just an integer: {:?}", (5u32));
+    // 解构元组
+    let tuple = (1, "hello", 4.5, true);
 
-    fn analyze_slice(slice: &[i32]) {
-        println!("first element of the slice: {}", slice[0]);
-        println!("the slice has {} elements", slice.len());
-    }
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
 
-    pub fn test_arr() {
-        let xs: [i32; 5] = [1, 2, 3, 4, 5];
-        println!("array occupies {} bytes", mem::size_of_val(&xs));
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{}", matrix);
+}
 
-        println!("borrow the whole array as a slice");
-        analyze_slice(&xs);
+fn analyze_slice(slice: &[i32]) {
+    println!("first element of the slice: {}", slice[0]);
+    println!("the slice has {} elements", slice.len());
+}
 
-        for i in 0..xs.len() + 1 {
-            // OOPS, one element too far
-            match xs.get(i) {
-                Some(xval) => println!("{}: {}", i, xval),
-                None => println!("Slow down! {} is too far!", i),
-            }
+pub fn test_arr() {
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
+
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
+
+    for i in 0..xs.len() + 1 {
+        // OOPS, one element too far
+        match xs.get(i) {
+            Some(xval) => println!("{}: {}", i, xval),
+            None => println!("Slow down! {} is too far!", i),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,41 @@
 #[allow(dead_code)]
 mod lib;
 
-use crate::lib::front_of_house::hosting;
-use crate::lib::structures::aaa;
-use crate::lib::enums::bbb;
-
-#[derive(Debug)]
-struct Structure(i32);
-#[derive(Debug)]
-struct Deep(Structure);
+use crate::lib::enums;
+use crate::lib::front_of_house;
+use crate::lib::structures;
 
 fn main() {
-    hosting::add_to_waitlist();
-    println!("Hello, world!");
-    println!("{} days", 31);
-    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
-    println!(
-        "{subject} {verb} {object}",
-        object = "the lazy dog",
-        subject = "the quick brown fox",
-        verb = "jumps over"
-    );
-    println!("Base 10 repr:               {}", 69420);
-    println!("Base 2 (binary) repr:       {:b}", 69420);
-    println!("Base 8 (octal) repr:        {:o}", 69420);
-    println!("Base 16 (hexadecimal) repr: {:x}", 69420);
-    println!("Base 16 (hexadecimal) repr: {:X}", 69420);
-    println!("{number:>5}", number = 1);
-    println!("{number:0>5}", number = 1);
-    println!("{number:0>width$}", number = 1, width = 5);
-    let number: f64 = 1.0;
-    let width: usize = 6;
-    println!("{number:>width$}");
-    println!("{:?} months in a year.", 12);
-    println!(
-        "{1:?} {0:?} is the {actor:?} name.",
-        "Slater",
-        "Christian",
-        actor = "actor's"
-    );
-    // `Structure` is printable!
-    println!("Now {:?} will print!", Structure(3));
-    println!("Now {:?} will print!", Deep(Structure(7)));
+    front_of_house::hello_world();
 
-    println!("My name is {0}, {1} {0}", "Bond", "James");
+    front_of_house::test_tuple();
+    front_of_house::test_arr();
+    structures::show_struct();
 
-    // Variables can be type annotated.
-    // 类型推断
-    let logical: bool = true;
-    let mut inferred_type = 12; // Type i64 is inferred from another line
-    inferred_type = 4294967296i64;
-    let mut mutable = 12; // Mutable `i32`
-    mutable = 21;
-    // 变量可以覆盖
-    let mutable = true;
-
-    hosting::test_tuple();
-    hosting::test_arr();
-    aaa::show_struct();
-
-    let top_left = aaa::Point { x: 1.0, y: 2.0 };
-    let bottom_right = aaa::Point { x: 2.0, y: 15.0 };
-    let _rectangle = aaa::Rectangle {
+    let top_left = structures::Point { x: 1.0, y: 2.0 };
+    let bottom_right = structures::Point { x: 2.0, y: 15.0 };
+    let _rectangle = structures::Rectangle {
         top_left: top_left,
         bottom_right: bottom_right,
     };
-    let res = aaa::rect_area(_rectangle);
+    let res = structures::rect_area(_rectangle);
     println!("res: {:?}", res);
 
     // 不能复用 top_left 变量因为被移动到_rectangle 里了。给 Point 结构体加上 copy 和 clone 的宏
     // https://doc.rust-lang.org/error-index.html#E0382
-    let res2 = aaa::square(top_left, 3.0);
+    let res2 = structures::square(top_left, 3.0);
     println!("res2: {:?}", res2);
 
-
-    let pressed = bbb::WebEvent::KeyPress('x');
+    let pressed = enums::WebEvent::KeyPress('x');
     // `to_owned()` creates an owned `String` from a string slice.
-    let pasted  = bbb::WebEvent::Paste("my text".to_owned());
-    let click   = bbb::WebEvent::Click { x: 20, y: 80 };
-    let load    = bbb::WebEvent::PageLoad;
-    let unload  = bbb::WebEvent::PageUnload;
+    let pasted = enums::WebEvent::Paste("my text".to_owned());
+    let click = enums::WebEvent::Click { x: 20, y: 80 };
+    let load = enums::WebEvent::PageLoad;
+    let unload = enums::WebEvent::PageUnload;
 
-    bbb::inspect(pressed);
-    bbb::inspect(pasted);
-    bbb::inspect(click);
-    bbb::inspect(load);
-    bbb::inspect(unload);
-
+    enums::inspect(pressed);
+    enums::inspect(pasted);
+    enums::inspect(click);
+    enums::inspect(load);
+    enums::inspect(unload);
 }
