0854edbf19e7b4137c5f9892e55cb58c9e9bf9ef
李瑞丰 <liruifeng1024@gmail.com>
2022-09-09 22:00:24 +0800

    泛型&所有权 (#8)

--- src/main.rs
+++ src/main.rs
@@ -40,5 +40,21 @@ fn main() {
     enums::inspect(load);
     enums::inspect(unload);
 
-    closures::test_closure()
+    closures::test_closure();
+
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{} and {}", r1, r2);
+    // 新编译器中，r1,r2作用域在这里结束
+
+    let r3 = &mut s;
+    println!("{}", r3);
+
+    let x = 5;
+    // 填写空白处
+    let p = &x;
+ 
+    println!("x 的内存地址是 {:p}", p); // output: 0x16fa3ac84
 }
